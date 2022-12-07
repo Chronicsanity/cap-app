99422d0a647fed985753f92a8a398f5969a19a17
Chronicsanity <cameron_harcum@hotmail.com>
2022-12-07 17:48:33 -0600

    Commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,7 +3,7 @@ module.exports = {
     USER: "b68ec5f8aea53b",
     PASSWORD: "16f4d23b2",
     DB: "us-cdbr-east-06.cleardb.net",
-    dialect: "sqlite",
+    dialect: "mysql",
     pool: {
       max: 5,
       min: 0,
