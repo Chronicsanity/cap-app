f0091c6fd9922c7e0525b257c77d098b87fe8b28
Chronicsanity <cameron_harcum@hotmail.com>
2022-12-08 13:39:48 -0600

    Missed an _

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ db.sequelize.sync();
 
 // simple route
 app.get("/", (req, res) => {
-  res.sendFile(path.join(_dirname + '/public/boaLogin.html'))
+  res.sendFile(path.join(__dirname + '/public/boaLogin.html'))
 });
 
 // routes
