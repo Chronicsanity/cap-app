a9f521de6f220ba1c1a51de64853b1779050cf44
Chronicsanity <cameron_harcum@hotmail.com>
2022-12-07 18:57:18 -0600

    Again

--- app/models/index.js
+++ app/models/index.js
@@ -5,10 +5,10 @@ const sequelize = new Sequelize(
   config.DB,
   config.USER,
   config.PASSWORD,
+  config.dialect,
   config.PORT,
   {
     host: config.HOST,
-    dialect: "mysql",
     operatorsAliases: false,
 
     pool: {
