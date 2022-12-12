c7d79cb728aba8e53407387f2a7e3361fa6366e6
Chronicsanity <cameron_harcum@hotmail.com>
2022-12-12 16:18:52 -0600

    r

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -51,7 +51,7 @@ exports.signin = async (req, res) => {
       return res.status(404).send({ message: "User Not found." });
     }
 
-    const passwordIsValid = bcrypt.compareSync(
+    const passwordIsValid = await bcrypt.compareSync(
       userPassword, req.body.password,
        
     );
