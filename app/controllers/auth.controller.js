eb2d72ce524bec40c446d5b7aa3746de0d8027ff
Chronicsanity <cameron_harcum@hotmail.com>
2022-12-13 16:39:38 -0600

    '

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -77,6 +77,23 @@ exports.signin = async (req, res) => {
   }
 };
 
+'use strict'
+
+class NoTimestamp {
+  register (Model) {
+    Object.defineProperties(Model, {
+      createdAtColumn: {
+        get: () => null,
+      },
+      updatedAtColumn: {
+        get: () => null,
+      },
+    })
+  }
+}
+
+module.exports = NoTimestamp
+
 exports.signout = async (req, res) => {
   try {
     req.session = null;
