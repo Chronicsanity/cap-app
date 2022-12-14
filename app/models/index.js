8eee6809fb95e0517b980f84966f8199203f4b38
Chronicsanity <cameron_harcum@hotmail.com>
2022-12-13 18:13:27 -0600

    g

--- app/models/index.js
+++ app/models/index.js
@@ -1,6 +1,5 @@
 const config = require("../config/db.config.js");
-
-const Sequelize = require("sequelize");
+import Sequelize from "sequelize";
 const sequelize = new Sequelize("mysql://b68ec5f8aea53b:6f4d23b2@us-cdbr-east-06.cleardb.net/heroku_a26e4a307a3f41f?reconnect=true", {
 
 logging: false
