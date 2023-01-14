84b158a3ccceb19f910f3401f02984d05b474ff8
Chronicsanity <cameron_harcum@hotmail.com>
2023-01-14 16:51:26 -0600

    f

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ logging: false
 
 const app = express();
 const db = require("./app/models");
-
+app.set('view engine','ejs')
 app.use(express.urlencoded({ extended: true }));
 app.use (
   session ({
