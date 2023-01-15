244bd5e80b5db7a410ce589ba4ec7c9f382f3191
Chronicsanity <cameron_harcum@hotmail.com>
2023-01-14 19:30:17 -0600

    f

--- server.js
+++ server.js
@@ -55,6 +55,8 @@ app.get("/")
 require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
 
+app.get("/forgetpass")
+require("./app/views/forgetpass.ejs")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
