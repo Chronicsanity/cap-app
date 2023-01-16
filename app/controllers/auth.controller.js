7985a5412e7f42167702844ed93542fe63596c3c
Chronicsanity <cameron_harcum@hotmail.com>
2023-01-16 14:41:12 -0600

    asdfasdfas

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -65,7 +65,7 @@ exports.signin = async (req, res, next) => {
     });
 
   if (!user) {
-      return res.status(404).ejs({username : false },
+      return res.status(404).send({message : "User not found" },
         await new Promise(resolve => setTimeout(resolve, 5000)),
       res.render('login') );
     }
