ecd5b97cbbcf872b47d49fee9ed8cfca464e77fd
Chronicsanity <cameron_harcum@hotmail.com>
2023-01-24 19:25:05 -0600

    fgsdfgsdf

--- server.js
+++ server.js
@@ -93,9 +93,9 @@ app.get('/dashboard', function(req, res){
             res.render('dashboard', {data : result})
           }
       });
-  };
+  };*/
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
-});*/
+});
