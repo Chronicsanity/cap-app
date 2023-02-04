3cde8667484517564d5682430faf7cbe8f63cb36
Chronicsanity <cameron_harcum@hotmail.com>
2023-02-04 12:34:11 -0600

    gfdfgdfgdfg

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -160,7 +160,7 @@ exports.signin = async (req, res) => {
     
     });*/
   
-res.render (('dashboard', scheduleTable()));
+res.render (('dashboard'));
     //return scheduleTable(req, res);
    /* return res.status(200).send({
       id: user.id,
