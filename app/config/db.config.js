b434e0af31c0872d737e48ef8b54ccd2062ebcdf
Chronicsanity <cameron_harcum@hotmail.com>
2023-02-16 16:09:40 -0600

    egege

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -57,10 +57,10 @@ module.exports = {
     }*/
   };
   const pool = mysql.createPool({
-    HOST: "ip-10-0-13-151",
+    HOST: "us-cdbr-east-06.cleardb.net",
     USER: "b68ec5f8aea53b",
   PASSWORD: "16f4d23b2",
-  DB: "us-cdbr-east-06.cleardb.net",
+  DB: "heroku_a26e4a307a3f41f",
   dialect: "mysql",
   PORT: "process.env.PORT || 8080",
   logging: false,
