823147d34495eeb06f3947127be483c906127378
Chronicsanity <cameron_harcum@hotmail.com>
2023-02-21 16:31:50 -0600

    rttty

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -228,7 +228,7 @@ res.render('dashboard', {data: result});
 
 app.get("data", (req, res, next) => {
 
-  req.query('SELECT * FROM users', function (err, data) {
+  app.query('SELECT * FROM users', function (err, data) {
 
     if (err) return res.status(400).send({ success: false, err },
       console.log (data));
