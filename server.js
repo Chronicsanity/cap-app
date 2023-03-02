a2e5e5f59bebe10aff30b0701729633276c55d57
Chronicsanity <cameron_harcum@hotmail.com>
2023-03-02 16:51:07 -0600

    ertewr

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cookieParser = require("cookie-parser");
 const db = require("./app/models");
 const User = db.user;
 const mysql = require('mysql');
-const {username, password} = require('./app/controllers/auth.controller')
+const {username, password, username2, password2} = require('./app/controllers/auth.controller')
 //const connection = require("../cap-capstone/app/config");
 const sequelize = new Sequelize("mysql://b68ec5f8aea53b:6f4d23b2@us-cdbr-east-06.cleardb.net/heroku_a26e4a307a3f41f?reconnect=true", {
 
