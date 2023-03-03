54f13caac4baee8372462001fae332963018777d
Chronicsanity <cameron_harcum@hotmail.com>
2023-03-02 19:59:46 -0600

    fgdf

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -226,12 +226,8 @@ res.render('dashboard', {data: result});
 
 exports.scheduleTable = async (req, res, next) => {
 
-  connection.connect(function (err){
-    if (err) { console.log (err);}
-    else {
-      console.log("Connected!");
-    }
-  });
+ 
+  
   pool.query('SELECT * FROM users',  function  (err, results) { 
     
     
