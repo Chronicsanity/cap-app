f1b1c3abed88db23bd72bacb30e738982b70abde
Chronicsanity <cameron_harcum@hotmail.com>
2023-03-04 19:07:03 -0600

    dfasdfas

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/index', (req, res) =>{
 
 });
 app.post('/data');
-app.get('/data', scheduleTable.results.bind(results));
+app.get('/data', scheduleTable.bind(results));
 
 
 app.use(express.static(__dirname + '/views'));
