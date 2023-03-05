7f2f9c3fb5a2fd3242e9346e9001c7797c593fa1
Chronicsanity <cameron_harcum@hotmail.com>
2023-03-04 22:11:15 -0600

    sadf

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cookieParser = require("cookie-parser");
 const db = require("./app/models");
 const User = db.user;
 const mysql = require('mysql');
-const {scheduleTable, results, Test}  = require('./app/controllers/auth.controller.js');
+const {scheduleTable, results, username, Test}  = require('./app/controllers/auth.controller.js');
 //const connection = require("../cap-capstone/app/config");
 const sequelize = new Sequelize("mysql://b68ec5f8aea53b:6f4d23b2@us-cdbr-east-06.cleardb.net/heroku_a26e4a307a3f41f?reconnect=true", {
 logging: false
@@ -81,7 +81,7 @@ app.get('/index', (req, res) =>{
 });
 app.post('/data');
 app.get('/data', (req, res) => {
-  res.render('data.ejs', console.log (scheduleTable.results.username))
+  res.render('data.ejs', console.log (username))
 });
 
 
