f07b8728cce9976bcc3c52af7abc883c455ca74a
Chronicsanity <cameron_harcum@hotmail.com>
2023-03-04 18:59:41 -0600

    asdfasd

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.get('/index', (req, res) =>{
 app.post('/data');
 app.get('/data', (req, res) =>{
 
-  const username2 = scheduleTable.result;
+  const username2 = scheduleTable.results;
   res.render('data.ejs', {username2: username2});
 });
 
