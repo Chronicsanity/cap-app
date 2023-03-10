26f6ef3ae8fa6ba5f9c0a1e2afaaea005df156a1
Chronicsanity <cameron_harcum@hotmail.com>
2023-03-09 21:15:13 -0600

    dfsd

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/index', (req, res) =>{
 
 });
 app.get('/data', async function (req, res) {
-  const username2 = scheduleTable().username;
+  const username2 = scheduleTable();
   res.render('data.ejs', {username2: username2} )
   });
 
