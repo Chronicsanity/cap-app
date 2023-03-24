a36bde8552cfab75594eb11967658cdb9d4108fd
Chronicsanity <cameron_harcum@hotmail.com>
2023-03-23 20:21:28 -0500

    wrfgwedg

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/index', (req, res) =>{
 
 });
 app.get('/data', async function (req, res) {
-scheduleTable(err, result)
+scheduleTable(result)
 if (err) {return err}
 if (!err && result) {
   return result
