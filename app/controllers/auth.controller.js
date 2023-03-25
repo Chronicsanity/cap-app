4f591db61ac0d62b2d2e30f5a657bedc90d4732a
Chronicsanity <cameron_harcum@hotmail.com>
2023-03-25 15:01:39 -0500

    gdfgsdfg

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -104,7 +104,7 @@ exports.scheduleTable = async function(){
     //console.log(data)
     console.log(result)
    
-    resolve (result);
+    return resolve (result);
     }).catch((error) => {
     console.error('Failed to retrieve data : ', error);
     
