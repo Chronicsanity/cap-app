893d3eb12f4aa7859f8af4dd0ff4ed6e128eed95
Chronicsanity <cameron_harcum@hotmail.com>
2023-03-25 15:12:41 -0500

    sdfa

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -97,7 +97,7 @@ exports.scheduleTable = async function(){
 
   
    await db.sequelize.sync().then(() => {
-    return new Promise ((resolve, reject) => {
+    return new Promise.all ((resolve, reject) => {
     User.findAll().then(res => {
     const result =  res;
     //const data = JSON.stringify(result);
