e8b8b4f3996156b7f070172c872e3549b9bc56b1
Chronicsanity <cameron_harcum@hotmail.com>
2023-03-27 14:52:14 -0500

    fdhgh

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const cookieParser = require("cookie-parser");
 const db = require("./app/models/index");
 const User = db.user;
 const mysql = require('mysql');
+const Promise = require('promise');
 //const  {scheduleTable}  = require('./app/controllers/auth.controller.js');
 const sequelize = new Sequelize("mysql://b68ec5f8aea53b:6f4d23b2@us-cdbr-east-06.cleardb.net/heroku_a26e4a307a3f41f?reconnect=true", {
 logging: false
@@ -70,7 +71,7 @@ app.get('/data', async function (req, res) {
   
  
   function scheduleTable() {
-return new promise (function(resolve, reject){
+return new Promise (function(resolve, reject){
     db.sequelize.sync().then(() => {
   
   
