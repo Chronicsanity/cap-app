81cb740c4a1ebdb94cf3da4bbab1d31c8ee2e224
Chronicsanity <cameron_harcum@hotmail.com>
2023-04-06 23:06:03 -0500

    wertwe

--- server.js
+++ server.js
@@ -115,7 +115,8 @@ app.post('/data', async function (req, res)  {
    //   throw 'Email "' + req.newName + '" is already registered';
   //}
   const user = new User(req);
- 
+ user.username = await req.body.username;
+ user.password = await req.body.password;
   // hash password
   //user.password = await bcrypt.hash(req.password, 10);
     user.id = await generateID(2, 10);
