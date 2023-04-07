a251534a489b19a117d9e54dfcdd101e07179f33
Chronicsanity <cameron_harcum@hotmail.com>
2023-04-06 22:31:40 -0500

    qewrtweq

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const db = require("./app/models/index");
 const User = db.user;
 const mysql = require('mysql');
 const Promise = require('promise');
+const bcrypt = require('bcryptjs')
 const sequelize = new Sequelize("mysql://b68ec5f8aea53b:6f4d23b2@us-cdbr-east-06.cleardb.net/heroku_a26e4a307a3f41f?reconnect=true", {
 logging: false
 });
