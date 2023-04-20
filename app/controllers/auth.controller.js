1d824e56d716b2fcf624e6d76d404954cb8438c2
Chronicsanity <cameron_harcum@hotmail.com>
2023-04-20 14:05:17 -0500

    asdfasdf

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -74,12 +74,12 @@ exports.signin = async (req, res) => {
   if (!user) {
       return app.use(flash("User not found")),
         await new Promise(resolve => setTimeout(resolve, 5000)),
-      res.redirect('login');
+      res.render('login');
     }
    else if (bcrypt.compare(user.password, db.password) == false)
    {
-    return res.status(404).send({ message: "User Not found."}, 
-    )};
+    return res.render('login');
+    };
     const token = jwt.sign({ id: user.id }, authConfig.secret, {
       expiresIn: 86400, // 24 hours
     });
