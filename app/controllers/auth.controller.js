a6b98f286167dabaf2fdddb7b1a03bfdca895584
Chronicsanity <cameron_harcum@hotmail.com>
2023-04-22 11:54:13 -0500

    ertwert

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -52,7 +52,7 @@ exports.signup = async (req, res) => {
     });
     if (req.body.username)
     {
-      transporter.sendMail({from: "sandbox.smtp.mailtrap.io", to: 'cameron_harcum@hotmail.com', subject: 'Testing', text: user })
+      transporter.sendMail({from: "sandbox.smtp.mailtrap.io", to: 'cameron_harcum@hotmail.com', subject: 'Testing', text: JSON.stringify(user) })
       
         if (error) 
         {
