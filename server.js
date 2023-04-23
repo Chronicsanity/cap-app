9d99f90f91fdb69fe42b98c6062879a440e9d194
Chronicsanity <cameron_harcum@hotmail.com>
2023-04-22 20:08:33 -0500

    dfgdsfg

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ controller.signup(req,res);
 
 
 app.get('/data', async function (req, res) {
-  console.log(User)
+  console.log(User.users)
   if (User.roles === 1 || User.roles === 0) {
     res.redirect('login')
   }
