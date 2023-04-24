c6f63558bc1ef164b6bf89e72f5c9c1505227809
Chronicsanity <cameron_harcum@hotmail.com>
2023-04-24 14:13:26 -0500

    asdfasdfasd

--- app/middleware/verifySignUp.js
+++ app/middleware/verifySignUp.js
@@ -1,9 +1,10 @@
-const { verifySignUp } = require(".");
 const db = require("../models");
 const ROLES = db.ROLES;
 const User = db.user;
 
-async function checkDuplicateUsernameOrEmail  (req, res, next) {
+
+module.exports = {
+async checkDuplicateUsernameOrEmail  (req, res, next) {
   try {
     // Username
     let user = await User.findOne({
@@ -37,24 +38,24 @@ async function checkDuplicateUsernameOrEmail  (req, res, next) {
       message: error.message
     });
   }
-};
+},
 
- async function checkRolesExisted (req, res) {
+ async checkRolesExisted (req, res) {
   if (req.body.roles) {
     for (let i = 0; i < req.body.roles.length; i++) {
       if (!ROLES.includes(req.body.roles[i])) {
         res.status(400).send({
+          auth: false,
           message: "Failed! Role does not exist = " + req.body.roles[i]
           
         });
-        return false;
+        return;
       }
-      else { return true;}
     }
   }
   
   next();
  
 }
- verifySignUp = {checkDuplicateUsernameOrEmail: checkDuplicateUsernameOrEmail, checkRolesExisted: checkRolesExisted} 
-module.exports = { verifySignUp }
+
+}
