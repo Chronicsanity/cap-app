d646bc8496c69a999ec6d7c6e69c84df6d6bf3e8
Chronicsanity <cameron_harcum@hotmail.com>
2023-04-24 14:21:30 -0500

    asdfasdfasd

--- app/middleware/verifySignUp.js
+++ app/middleware/verifySignUp.js
@@ -40,7 +40,7 @@ async checkDuplicateUsernameOrEmail  (req, res, next) {
   }
 },
 
- async checkRolesExisted (req, res) {
+ async checkRolesExisted (req, res, next) {
   if (req.body.roles) {
     for (let i = 0; i < req.body.roles.length; i++) {
       if (!ROLES.includes(req.body.roles[i])) {
