225ba4a95b0c50ffa8403a44a9432940c8d7aa79
Chronicsanity <cameron_harcum@hotmail.com>
2023-05-08 16:41:10 -0500

    ertherth

--- server.js
+++ server.js
@@ -211,7 +211,7 @@ if (await req.body.hasOwnProperty("accept")){
         username: newUser.username,
         password: newUser.password,
         email: newUser.email,
-        roles: newUser.roles
+        role: newUser.roles
       })
      QueuedUser.destroy({
 
