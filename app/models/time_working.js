2cd5cae6b8e98d5881dc11ac4de2f13c2e988d11
Chronicsanity <cameron_harcum@hotmail.com>
2023-05-09 13:20:14 -0500

    fgdfghdf

---
+++ app/models/time_working.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, Sequelize) => {
+	const Time_Working = sequelize.define("time_working", {
+	  id: {
+		type: Sequelize.INTEGER,
+        primaryKey: true
+	  },
+	  date: {
+		type: Sequelize.DATETIME
+	  },
+	  starting_at: {
+		type: Sequelize.INTEGER
+	  },
+	  ending_at: {
+		type: Sequelize.INTEGER,
+		
+	}, 
+
+	},{
+		timestamps: false
+	});
+  
+	return Time_Working;
+  };
