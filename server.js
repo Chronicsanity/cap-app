a5aed859ed61cb1a4917f7adcac676aa4db32979
Chronicsanity <cameron_harcum@hotmail.com>
2023-05-25 14:23:14 -0500

    gwergwer

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ app.post('/data', (req, res) => {
   //}
   const user = await req.body.username
   const addEmployee = await User.findOne({
-      where: {User: user}
+      where: {user: user}
       })
       if (addEmployee == null)
       {
