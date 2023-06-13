2b2e0e2077941ae204ee7c0be6c10f2407d7e15e
Chronicsanity <cameron_harcum@hotmail.com>
2023-06-13 12:24:11 -0500

    fsdfgsdfg

--- app/models/calendar.js
+++ app/models/calendar.js
@@ -12,14 +12,8 @@ module.exports = function (date) {
 
       const domContainer = document.querySelector('#calendar_container');
       const root = ReactDOM.createRoot(domContainer);
-      
-      app.get('/schedule', async (req, res) =>{
-  
-       
-          res.render ('schedule', e(Calendar), {date: date}, {setDate:setDate})
-        
-        })
-     // root.render(e(Calendar), {date: date}, {setDate:setDate});
+   
+     root.render(e(Calendar), {date: date}, {setDate:setDate});
       
     }
   
