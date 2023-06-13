d84d1d9232ca6feca28b67945b505a320600ca7c
Chronicsanity <cameron_harcum@hotmail.com>
2023-06-13 12:13:05 -0500

    rtyert

--- app/models/calendar.js
+++ app/models/calendar.js
@@ -6,9 +6,10 @@ import Calendar from 'react-calendar';
 
 module.exports = function (calendar) {
     const [date, setDate] = useState(new Date())
+    const currDate = Date.now;
       const domContainer = document.querySelector('#calendar_container');
       const root = ReactDOM.createRoot(domContainer);
-      root.render(e(Calendar), e(date), e(setDate));
+      root.render(e(Calendar), {date: currDate}, {setDate:setDate});
       
     }
   
