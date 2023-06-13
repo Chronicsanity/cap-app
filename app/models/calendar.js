74ec55100ad3d270607cd6ebc704ef4d25f6bfa9
Chronicsanity <cameron_harcum@hotmail.com>
2023-06-13 13:19:53 -0500

    hjteryety

--- app/models/calendar.js
+++ app/models/calendar.js
@@ -15,7 +15,7 @@ console.log (date)
       const domContainer = document.querySelector('#calendar_container');
       const root = ReactDOM.createRoot(domContainer);
    
-     root.render(e(Calendar), {date: date}, {setDate:setdate});
+     root.render(e(Calendar), {date: date});
       
     }
   
