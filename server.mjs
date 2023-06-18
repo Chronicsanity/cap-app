6aedb555ca5a8ce36305ad37b96023a1da8de43b
Chronicsanity <cameron_harcum@hotmail.com>
2023-06-18 11:58:31 -0500

    wergwerg

--- server.mjs
+++ server.mjs
@@ -114,7 +114,7 @@ if (req.body.hasOwnProperty("acceptEmployee")) {
   async function createEmployee(req) {
    
   const user = JSON.stringify(req.body.employee_name).replace(/]|[[]/g, '');
-  const editEmployee = await Employee.findOne({where: {user: user}});
+  const editEmployee = await Employee.findAll({where: {user: user}});
 
      if (user == null || user =='""')
       {
