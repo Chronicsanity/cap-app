338a4aaf2bb4122c08ea421413af058f10c67015
Chronicsanity <cameron_harcum@hotmail.com>
2023-06-18 13:04:13 -0500

    erthert

--- app/models/employee.js
+++ app/models/employee.js
@@ -1,4 +1,4 @@
-module.exports = (sequelize, Sequelize) => {
+module.exports = (sequelize, Sequelize, DataTypes) => {
 	const Employee = sequelize.define("employee_table", {
 		id: {
 			type: Sequelize.INTEGER,
