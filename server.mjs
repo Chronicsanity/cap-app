9ab1c7b38777607d8e0ec309ce40c3ab449e7a5f
Chronicsanity <cameron_harcum@hotmail.com>
2023-07-28 17:18:00 -0500

    ertwert

--- server.mjs
+++ server.mjs
@@ -17,7 +17,7 @@ import Promise from 'promise';
 import bcrypt from 'bcryptjs';
 import controller from "./app/controllers/auth.controller.js";
 import flash from 'connect-flash';
-//import time_picker from './tui-time-picker'
+import time_picker from './app/node_modules/tui-time-picker'
 const sequelize = new Sequelize("mysql://b68ec5f8aea53b:6f4d23b2@us-cdbr-east-06.cleardb.net/heroku_a26e4a307a3f41f?reconnect=true", {
 logging: false
 });
@@ -287,8 +287,8 @@ const scheduleInfo = App;
 app.use(express.static(__dirname + '/views'));
 
 app.get('/shiftmaker', async (req, res) => {
-//const time = time_picker
-  res.render ('shiftmaker'/*, {time:time}*/ )
+const time = time_picker
+  res.render ('shiftmaker', {time:time} )
 
 })
 
