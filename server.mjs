4441010d63c24d07c9e88c6e55bf65792beeee31
Chronicsanity <cameron_harcum@hotmail.com>
2023-07-30 20:38:48 -0500

    rthesdfsr

--- server.mjs
+++ server.mjs
@@ -311,10 +311,14 @@ if(Employee.findOne( {where: {user: check}})) {
   start.push(req.body.time1);
   end.push(req.body.time2);
   name.push(check)
-  Shift.employee_name = await JSON.stringify(name);
-  Shift.start_time = await JSON.stringify(start);
-  Shift.end_time = await JSON.stringify(end);
-  console.log(Shift.end_time)
+  
+
+  Shift.upsert({
+    employee_name: name,
+    start_time: start,
+    end_time: end
+  })
+  console.log(Shift)
 console.log(check+" is set for "+start+" to "+end)
 }
  
