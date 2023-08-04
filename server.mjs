e75a91eff6ef76186d96f0030213577e17d7e768
Chronicsanity <cameron_harcum@hotmail.com>
2023-08-03 19:40:21 -0500

    rthertf

--- server.mjs
+++ server.mjs
@@ -296,7 +296,7 @@ app.get('/shiftmaker', async (req, res) => {
   const job_list = await Jobs.findAll();
 const data = employee_list;
 const jobData = job_list;
-
+console.log(data)
   res.render ('shiftmaker',{data: data, jobData:jobData} )
   })
 app.post('/shiftmaker', async (req, res) =>{
