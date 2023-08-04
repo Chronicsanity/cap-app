1b61b6377907855ada16d124b50cde5b595376c3
Chronicsanity <cameron_harcum@hotmail.com>
2023-08-03 19:47:11 -0500

    ertherther

--- server.mjs
+++ server.mjs
@@ -296,7 +296,7 @@ app.get('/shiftmaker', async (req, res) => {
   const job_list = await Jobs.findAll();
 const data = employee_list;
 const jobData = job_list;
-console.log(jobData)
+console.log(job_list+jobData)
   res.render ('shiftmaker',{data: data, jobData:jobData} )
   })
 app.post('/shiftmaker', async (req, res) =>{
