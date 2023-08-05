0707e009a556808d8d462c9e659ad33689b22bad
Chronicsanity <cameron_harcum@hotmail.com>
2023-08-05 17:28:59 -0500

    rtyert

--- server.mjs
+++ server.mjs
@@ -340,8 +340,7 @@ const jobData = job_list;
 
 if (confirmed_job.job_value > confirmed_Employee.job_value) { 
   const data = employee_list
-  res.status(404).send({ message: "They are not trained for this job yet!"})
- res.redirect('shiftmaker')
+  res.status(404).send("They are not trained for this job yet!")
 }
 
 else{
