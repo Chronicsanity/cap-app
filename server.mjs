05a42440966a19dbabffb70fc50b7d8f3ddee1b1
Chronicsanity <cameron_harcum@hotmail.com>
2023-08-05 19:52:30 -0500

    thjertjhetrherthjetrgyhjedrtghjfghjmfgh

--- server.mjs
+++ server.mjs
@@ -350,7 +350,6 @@ async function generateID(min, max) {
 if (confirmed_job.job_value > confirmed_Employee.job_value) { 
   const data = employee_list
    const jobData = job_list;
-  res.status(404).send("They are not trained for this job yet!")
   return (res.status(404).send("They are not trained for this job yet!"))
 }
 
