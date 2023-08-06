ba72a3b767263f3ea2b06fb334096ad3b7bc67ca
Chronicsanity <cameron_harcum@hotmail.com>
2023-08-05 21:01:12 -0500

    ewrthewrhsdfgargwe

--- server.mjs
+++ server.mjs
@@ -371,7 +371,7 @@ else{
   console.log("1")
 }}
 else{
-  console.log("2")
+  console.log(Shift.employee_name+" "+confirmed_Employee+" "+req.body.datetimes+" "+shiftDate)
    Shift.create({
     shiftID: id,
      employee_name: name,
