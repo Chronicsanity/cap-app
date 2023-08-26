aa5c6cd28293c53338f922a4f3ed3952b310167c
Chronicsanity <cameron_harcum@hotmail.com>
2023-08-26 15:59:22 -0500

    rtherther

--- server.mjs
+++ server.mjs
@@ -459,8 +459,14 @@ app.post('/shiftassignment', async function (req,res){
 const amount = req.body.amntEmployees
 const assignments = req.body.assignments;
 const weekcheck = JSON.stringify(req.body.weekcheck).replace(/]|[/""[]/g, "")
+const result = [];
+for (var i = 0; i < weekcheck.length; i++){
+  result.push(
+    weekcheck[i]
+  )
+}
 
-console.log(weekcheck, "Mon AM")
+console.log(weekcheck, result)
 if (weekcheck === "Mon AM")
 {
   await Shift_Assignments.update({
