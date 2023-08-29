7e837dc5da3c93bcb0e64cb9f139469957a96544
Chronicsanity <cameron_harcum@hotmail.com>
2023-08-29 15:24:43 -0500

    hertyht

--- server.mjs
+++ server.mjs
@@ -469,7 +469,7 @@ for (var i=0; i<weekcheck.length; i+=test) {
 
 
 console.log(weekcheck, check, amount, assignments)
-if (weekcheck === "Tue AM")
+if (weekcheck === "Mon AM")
 {
   await Shift_Assignments.update({
     Assignments: assignments,
