2e57231aff032a311b8b7759d8488f55e31c8c9d
Chronicsanity <cameron_harcum@hotmail.com>
2023-08-29 18:18:28 -0500

    r234t234

--- server.mjs
+++ server.mjs
@@ -468,7 +468,7 @@ for (var i=0; i<weekcheck.length; i+=test) {
 }
 
 
-console.log(weekcheck, amount.length, assignments.length, amount, assignments)
+console.log(weekcheck, amount.length, assignments.length, amount[1], assignments)
 if (check.indexOf("Mon AM") === 0)
 {
   if (amount.length > 1 || assignments.length > 1)
