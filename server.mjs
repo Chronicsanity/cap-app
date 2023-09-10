e6c6bc70fcabba71d38a52f7569b4ce8c875191b
Chronicsanity <cameron_harcum@hotmail.com>
2023-09-09 21:53:53 -0500

    hetrhert

--- server.mjs
+++ server.mjs
@@ -411,7 +411,7 @@ if (await controller.dayChecker("Mon PM") > 0)
 }
 
    const PM = "PM"
- console.log(check+" is set for "+start+" to "+end+ " at "+datetime+" "+id+" "+datetime.includes(PM))}
+ console.log(check+" is set for "+start+" to "+end+ " at "+datetime+" "+id+" "+start.includes(PM))}
 
  const employee_list = await Employee.findAll();
  const job_list = await Jobs.findAll();
