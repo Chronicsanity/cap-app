78b1a6434eac1eafc140296125baa526fc76c140
Chronicsanity <cameron_harcum@hotmail.com>
2023-09-09 20:10:54 -0500

    ertherghgrh

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -398,6 +398,7 @@ exports.scheduleTable = async function (res) {
 
               exports.dayChecker = async function (day) {
                 const amountPull = await Shift_Assignments.findOne( {where: {DaysAssigned: day}})
+                console.log(amountPull)
               return amountPull
             }
 exports.signin = async (req, res) => {
