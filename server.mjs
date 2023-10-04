303e6f9d03880892d68315d96edec5154c26ab81
Chronicsanity <cameron_harcum@hotmail.com>
2023-10-04 18:30:41 -0500

    gwergew

--- server.mjs
+++ server.mjs
@@ -366,7 +366,7 @@ const fixedName = JSON.stringify(check).replace(/[\{\}\"]/g, "");
 const conValue = confirmed_job.job_value;
 const EmpValue = confirmed_Employee.job_value
   async function dayChecker (day) {
-  const amountPull = await Shift_Assignments.findOne( {where: {DaysAssigned: day}})
+  const amountPull = await Shift_Assignments.findOne( {where: {DaysAssigned: "Mon AM"}})
   console.log(amountPull)
 return amountPull
 }
