2a2ba0c3d9bf9b584d272049dec1eed82f87ae50
Chronicsanity <cameron_harcum@hotmail.com>
2023-10-16 20:16:28 -0500

    rtherbr

--- server.mjs
+++ server.mjs
@@ -748,7 +748,7 @@ if (datetime.indexOf("Tuesday")>= -1 && start.toString().indexOf("AM") > -1) {
                       }
 
 
-const ahhhh = await Shift_Assignments.findAll( {where: {DaysAssigned: "Mon AM"}},{attributes: {exclude: ['id', 'Assignments', 'DaysAssigned', 'min_title']}}).then(result => {return result})
+const ahhhh = await Shift_Assignments.findAll( {attributes: {exclude: ['id', 'Assignments', 'DaysAssigned', 'min_title']}},{where: {DaysAssigned: "Mon AM"}}).then(result => {return result})
 const fixed = JSON.stringify(ahhhh)
 
  console.log(check+" is set for "+start+" to "+end+ " at "+datetime+" "+id+" "+datetime.indexOf("Monday")+start.toString().indexOf("AM")+" "+ fixed)}
