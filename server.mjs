b373a8caf5b605bbefa900f461be4f46fcb1557a
Chronicsanity <cameron_harcum@hotmail.com>
2023-10-17 15:01:41 -0500

    tyetryhety

--- server.mjs
+++ server.mjs
@@ -874,7 +874,7 @@ if (check.indexOf("Mon AM") == 0)
   
   i++
   }
-  else{await Shift_Assignments.destroy({where:{DaysAssigned: "Mon AM", AmntEmp: null}})
+  else if (shifting == null) {await Shift_Assignments.destroy({where:{DaysAssigned: "Mon AM", AmntEmp: null}})
   i++
   }}
   
