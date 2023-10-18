00958159d9efc04ad577a9f6800ceed5b5a7682f
Chronicsanity <cameron_harcum@hotmail.com>
2023-10-17 19:51:29 -0500

    etyjnhety

--- server.mjs
+++ server.mjs
@@ -849,7 +849,7 @@ if (min_title == "busser")
   valcounter.push("1")
 }
 if (Shift_Assignments.id == null) {
-  for (var i=0; i< check.length;) {
+  for (var i=0; i< amount;) {
 
 
 if (check.indexOf("Mon AM") == 0)
