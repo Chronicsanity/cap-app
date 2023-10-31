e387aefb82a5256d14ae2500ee8808e00ce434a8
Chronicsanity <cameron_harcum@hotmail.com>
2023-10-31 17:47:35 -0500

    tynjteyhnt

--- server.mjs
+++ server.mjs
@@ -889,35 +889,35 @@ app.post('/createAssignments', async function (req,res){
 
 const newJobs = await req.body.assignments;
 const newMinTitle = JSON.stringify(await req.body.min_title).replace(/]|[[]/g, '');
+const test = []
 
-
-
-const test = await Jobs.upsert({
-job_value: newMinTitle,
-jobs: newJobs
-})
-const test2 = await test.min_title;
 if(newMinTitle == "4")
 {
-  await test2.push(JSON.stringify("Head Chef"))
+  test.push(JSON.stringify("Head Chef"))
 }
 if(newMinTitle == "3")
 {
-  await test2.push ("Sous Chef")
+  test.push ("Sous Chef")
 }
 if(newMinTitle == "2")
 {
-  await test2.push ("Line Cook")
+  test.push ("Line Cook")
 }
 if(newMinTitle == "1")
 {
- await test2.push ("Busser")
+  test.push ("Busser")
 }
 if(newMinTitle == "0")
 {
- await test.min_title == "Busser"
+  test.min_title == "Busser"
 }
-console.log(await test, await test2)
+ await Jobs.upsert({
+job_value: newMinTitle,
+jobs: newJobs
+})
+
+
+console.log( test)
 
 await controller.jobList(res).then(info => {
   res.render('createAssignments', {jobs:info})
