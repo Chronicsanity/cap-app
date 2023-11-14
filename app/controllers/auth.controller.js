f59cb1e457e344b5634ac00494bff2f605c016df
Chronicsanity <cameron_harcum@hotmail.com>
2023-11-14 15:50:55 -0600

    rnytnrtyn

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -447,7 +447,7 @@ exports.scheduleTable = async function (res) {
               
               exports.testing = async (req) => {
                 const date = await req.body.datetimes;
-
+                console.log (date)
                 if (date.indexOf("Mon") >= 1)
                 {
                   return "Mon"
