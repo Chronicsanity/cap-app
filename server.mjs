fbde49bb40a3d1b18ae4fc91099ce1f872fc296f
Chronicsanity <cameron_harcum@hotmail.com>
2023-11-13 20:15:42 -0600

    brbrtgb

--- server.mjs
+++ server.mjs
@@ -602,7 +602,7 @@ if (await chosenDay != null)
 {
   if (await req.body.hasOwnProperty("add")){
     console.log(test, test.indexOf(dayPicked))
-    if(await counter == await dayCounter && test.indexOf(await dayPicked) == -1){
+    if(await counter == await dayCounter && test.indexOf(await dayPicked) == 1){
       console.log(test.indexOf(await dayPicked))
     await Shift_Assignments.update({Assignments:jobPicked, min_title:title_update}, {where:{DaysAssigned:dayPicked}&&{Shift_counter:counter},})}
       
