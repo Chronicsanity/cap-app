e926fccc7010c6e6a831d169fa38495ecab5e6ad
Chronicsanity <cameron_harcum@hotmail.com>
2023-11-16 18:04:11 -0600

    thwrthwr

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -147,7 +147,7 @@ exports.scheduleTable = async function (res) {
           
          const object = res
           console.log(object, 1)
-          if (object == null || object == undefined || object == "[]" || object == []) {
+          if (object.length == 0) {
             object.id =  JSON.stringify(id).replace(/]|[[]/g, '')
             object.username =  JSON.stringify(username).replace(/]|[[]/g, '')
             object.email =  JSON.stringify(email).replace(/]|[[]/g, '')
