2627ad331144d893a5b7e6bd80ee293a3c47ad26
Chronicsanity <cameron_harcum@hotmail.com>
2023-11-28 21:08:27 -0600

    wrethgwer

--- server.mjs
+++ server.mjs
@@ -711,7 +711,7 @@ app.get('/shiftSchedule', async function (req,res){
      
  
     for (var i = 0; i < info.length; i++) {
-      const test = info.date
+      const test = info[i].date
       const attempt = Object.fromEntries(info.map(info => [test, info]))
    console.log(attempt)
     }
