a78cb1c42f7ca1c93abce7c4c38f717cecba1282
Chronicsanity <cameron_harcum@hotmail.com>
2023-11-29 19:52:49 -0600

    rtgnbertg

--- server.mjs
+++ server.mjs
@@ -708,10 +708,13 @@ if (await chosenDay != null)
 })
 app.get('/shiftSchedule', async function (req,res){ 
   await controller.shiftSchedule(res).then(info => {
-     
-    info.forEach((info) =>{
-      console.log (info.date)
-    })
+    const test = []
+    for (var i = 0; i < info.length; i++) {
+   console.log(info[i].indexOf("Mon"))
+    }
+    
+
+    
    
         
     
