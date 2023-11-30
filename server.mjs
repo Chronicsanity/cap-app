201fdd073541e74f955c6bfb3ec905fe2add71c8
Chronicsanity <cameron_harcum@hotmail.com>
2023-11-29 21:08:38 -0600

    ertherthert

--- server.mjs
+++ server.mjs
@@ -762,7 +762,7 @@ app.get('/shiftSchedule', async function (req,res){
   else if (test.indexOf("Sunday PM") >= 1 ){
     
   }
-  console.log(info.findOne({ where: {date: "Monday AM"}}))
+  
   res.render('shiftSchedule', {info: info}
   )
     }
